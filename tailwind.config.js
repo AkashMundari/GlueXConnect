54c1716db04fb279d296fc0e5e3c7794c6b5c8f8
AkashMundari <153832855+AkashMundari@users.noreply.github.com>
2025-08-24 20:37:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/App.jsx",
-    "./src/components/SwapForm.jsx",
-    "./src/components/SwapChatInterface.jsx","./src/components/ChatInterface.jsx","./src/components/Searching.jsx",
+    "./src/components/FinalChatInterface.jsx",
   ],
   theme: {
     extend: {},
